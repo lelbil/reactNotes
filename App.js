b8954b908bec8bf22630a0c2706a79d2430021be
Billel Attouchi <billel.attouchi@ekino.com>
2018-04-20 20:59:04 +0200

    default style for navigation bar

--- App.js
+++ App.js
@@ -5,11 +5,6 @@
  */
 
 import React, {Component} from 'react';
-import {
-    View,
-    Text,
-    StyleSheet
-} from 'react-native';
 import { StackNavigator } from 'react-navigation'
 
 import NoteScreen from './App/Components/NoteScreen'
@@ -21,24 +16,22 @@ const home = props => {
 
 
 home.navigationOptions = {
-    title: 'Home Screen'
+    title: 'Home Screen',
 }
 
 const SimpleApp = StackNavigator(
     {
         Home: { screen: home },
         SecondScreen: { screen: NoteScreen, title: "Second Screen" }
-        //Home: { screen: NoteScreen, title: "Second Screen" }
+    },
+    {
+        navigationOptions: {
+            headerTintColor: 'white',
+            headerStyle: {
+                backgroundColor: '#5B29C1',
+            }
+        }
     }
 )
 
 export default SimpleApp
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'rgb(30, 230, 230)',
-    },
-})
